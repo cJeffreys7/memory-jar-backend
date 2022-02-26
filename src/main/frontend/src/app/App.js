98c009c765a1f6a9a8a2bf4267e3dd07f816697d
Chris Jeffreys <chrisbjeffreys@gmail.com>
2022-02-26 10:54:40 -0600

    refactored UserProfiles page

--- src/main/frontend/src/app/App.js
+++ src/main/frontend/src/app/App.js
@@ -1,75 +1,7 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import axios from 'axios';
-import { useDropzone } from 'react-dropzone';
+import React from 'react';
+import UserProfiles from '../pages/UserProfiles';
 
 import './App.css';
-
-const UserProfiles = () => {
-  
-  const [userProfiles, setUserProfiles] = useState([]);
-
-  const fetchUserProfiles = () => {
-    axios.get("http://localhost:8080/api/user-profile")
-    .then(res=> {
-      console.log(res);
-      setUserProfiles(res.data);
-    })
-  }
-
-  useEffect(() => {
-    fetchUserProfiles();
-  }, []);
-
-  return userProfiles.map((userProfile, index) => {
-    return (
-      <div key={index}>
-        {userProfile.userProfileId ? <img src={`http://localhost:8080/api/user-profile/${userProfile.userProfileId}/image/download`} /> : null}
-        <br/>
-        <br/>
-        <h1>{userProfile.username}</h1>
-        <p>{userProfile.userProfileId}</p>
-        <Dropzone {...userProfile} />
-        <br/>
-      </div>
-    )
-  });
-}
-
-function Dropzone({ userProfileId }) {
-  const onDrop = useCallback(acceptedFiles => {
-    const file = acceptedFiles[0];
-    console.log(file);
-    const formData = new FormData();
-    // file param must be the same as api param
-    formData.append("file", file);
-    axios.post(
-      `http://localhost:8080/api/user-profile/${userProfileId}/image/upload`,
-      formData,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data"
-        }
-      }
-    ).then(() => {
-      console.log('file uploaded successfully');
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
-
-  return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
-    </div>
-  )
-}
 
 function App() {
   return (
---
+++ src/main/frontend/src/pages/UserProfiles/index.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Dropzone from '../../components/Dropzone';
+
+const UserProfiles = () => {
+    const [userProfiles, setUserProfiles] = useState([]);
+
+    const fetchUserProfiles = () => {
+        axios.get("http://localhost:8080/api/user-profile")
+        .then(res=> {
+        console.log(res);
+        setUserProfiles(res.data);
+        });
+    }
+
+    useEffect(() => {
+        fetchUserProfiles();
+    }, []);
+    return userProfiles.map((userProfile, index) => {
+        return (
+            <div key={index}>
+                {userProfile.userProfileId ? <img src={`http://localhost:8080/api/user-profile/${userProfile.userProfileId}/image/download`} /> : null}
+                <br/>
+                <br/>
+                <h1>{userProfile.username}</h1>
+                <p>{userProfile.userProfileId}</p>
+                <Dropzone {...userProfile} />
+                <br/>
+            </div>
+        )
+    });
+};
+
+export default UserProfiles;
