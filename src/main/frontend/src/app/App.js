d9c04a766a9fafae4a493977d163cb88d93e3600
Chris Jeffreys <chrisbjeffreys@gmail.com>
2022-02-28 10:23:46 -0600

    refactor CSS files to SCSS

--- src/main/frontend/src/app/App.js
+++ src/main/frontend/src/app/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import UserProfiles from '../pages/UserProfiles';
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
