d919cb5e86b880f81af69f7ed5f61611b7f241a7
Chris Jeffreys <chrisbjeffreys@gmail.com>
2022-02-28 10:16:20 -0600

    add IconButton component

---
+++ src/main/frontend/src/components/MUI/IconButton/index.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import { ThemeProvider } from '@mui/material';
+
+function IconButton({ theme, color, icon }) {
+    const [pressed, setPressed] = useState(false);
+
+    const handleClick = () => {
+        setPressed(!pressed);
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Button color={pressed ? "primary" : "secondary"} onClick={handleClick}>
+                {icon && icon}
+            </Button>
+        </ThemeProvider>
+    );
+}
+
+export default IconButton;
--- src/main/frontend/src/components/Memory/index.jsx
+++ src/main/frontend/src/components/Memory/index.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 
-import defaultImg from '../../assets/memoryjar_logo_dark.svg'
+import defaultImg from '../../assets/memoryjar_logo_dark.svg';
+import { createTheme } from '@mui/material';
+import IconButton from '../MUI/IconButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 import './styles.scss'
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#c43232'
+        },
+        secondary: {
+            main: '#a0a0a0'
+        }
+    }
+})
+
+const configIconButton = {
+    theme: theme,
+    icon: <FavoriteIcon />
+}
 
 const Memory = () => {
     return (
         <div className='wrapper'>
             <div className='border'>
                 <img src={defaultImg} alt="Memory Jar Icon" />
+                <IconButton {...configIconButton}/>
             </div>
         </div>
     );
