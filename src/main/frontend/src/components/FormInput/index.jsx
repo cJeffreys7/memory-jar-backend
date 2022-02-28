b9b21a4b8d72f50b4f7690c366001d90eec9e4c5
Chris Jeffreys <chrisbjeffreys@gmail.com>
2022-02-28 17:02:13 -0600

    add SignIn validation

--- src/main/frontend/src/components/FormInput/index.jsx
+++ src/main/frontend/src/components/FormInput/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { TextField } from '@mui/material';
 
-const FormInput = ({ className, name, type, placeholder, value, onChange }) => {
+const FormInput = ({ className, name, type, label, placeholder, error, helperText, value, onChange, variant }) => {
     return (
         <div className='input-wrapper'>
             <div className='input-border'>
-                <input
+                <TextField
                     className={className}
                     name={name}
                     type={type}
+                    label={label}
                     placeholder={placeholder}
+                    error={error}
+                    helperText={helperText}
                     value={value}
                     onChange={onChange}
+                    variant={variant}
                 />
             </div>
         </div>
--- src/main/frontend/src/pages/SignIn/index.jsx
+++ src/main/frontend/src/pages/SignIn/index.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // components
 import FormInput from '../../components/FormInput';
 import Button from '../../components/MUI/StyledButton';
 
+// assets
 import Icon from '../../assets/memoryjar_icon.svg'
 import LogoText from '../../assets/memoryjar_logo_text_dark.svg'
 
@@ -14,22 +15,191 @@ const initialFormData = {
     password: ''
 }
 
+const initialErrors = {
+    emailEntry: false,
+    emailError: false,
+    emailHelperText: 'Invalid email',
+    passwordEntry: false,
+    passwordError: false,
+    passwordHelperText: 'Invalid password'
+}
+
+// const initialValidation = {
+//     validationDelay: {},
+//     isDelayed: false,
+//     delayRate: 500
+// }
+
 const SignIn = (props) => {
     const [formData, setFormData] = useState(initialFormData);
+    const [errors, setErrors] = useState(initialErrors);
+    // const [validation, setValidation] = useState(initialValidation);
+
+    // let validationDelays = {};
+    // let isDelayed = false;
+    // let delayRate = 1000;
 
     const handleChange = e => {
         setFormData({
             ...formData,
             [e.target.name]: e.target.value
-        })
+        });
+        if (e.target.name === 'email') {
+            setErrors({
+                ...errors,
+                emailEntry: true
+            })
+        }
+        if (e.target.name === 'password') {
+            setErrors({
+                ...errors,
+                passwordEntry: true
+            })
+        }
     }
 
     const handleSubmit = e => {
         e.preventDefault();
         setFormData(initialFormData);
+        setErrors(initialErrors);
     }
 
     const { email, password } = formData;
+    const { emailEntry, emailError, emailHelperText, passwordEntry, passwordError, passwordHelperText } = errors;
+    // const { validationDelay, isDelayed, delayRate } = validation;
+
+
+    useEffect(() => {
+        const formCheck = async => {
+            let errors = [];
+            if (emailEntry) {
+                errors.push({ emailEntry: true });
+                errors.push(emailValidation());
+            }
+            if (passwordEntry) {
+                errors.push({ passwordEntry: true });
+                errors.push(passwordValidation());
+            }
+            let formErrors = {};
+            errors.forEach(error => {
+                formErrors = {
+                    ...formErrors,
+                    ...error
+                }
+            })
+            setErrors({
+                ...errors,
+                ...formErrors
+            })
+        }
+
+        formCheck();
+    }, [email, password]);
+
+    // const inputCheck = (validationMethods) => {
+    //     if (isDelayed) {
+    //         interruptValidationDelay()
+    //     }
+    //     isDelayed = true;
+    //     validationDelay = setTimeout(() => {
+    //         for (const method of validationMethods) {
+    //             method();
+    //         }
+    //         // validationMethod();
+    //         isDelayed = false;
+    //     }, delayRate);
+    // }
+
+    // const emailInputCheck = () => {
+    //     if (isDelayed) {
+    //         interruptValidationDelay('email')
+    //     }
+    //     isDelayed = true;
+    //     const emailValidationDelay = setTimeout(() => {
+    //         isDelayed = false;
+    //         return emailValidation();
+    //     }, delayRate);
+    //     validationDelays = {
+    //         ...validationDelays,
+    //         email: emailValidationDelay
+    //     }
+    //     console.log('Delay timers:', validationDelays);
+    // }
+
+    const emailValidation = () => {
+        console.log('Validating email...');
+        let error = {};
+        if (!email) {
+            console.log('No email entered');
+            error = {
+                emailError: true,
+                emailHelperText: 'Please enter your email'
+            }
+        } else {
+            const emailExpression = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
+            const emailRegExp = new RegExp(emailExpression)
+            if (email.match(emailRegExp)) {
+                console.log('Email matches email pattern');
+                error = {
+                    emailError: false
+                }
+            } else {
+                console.log('Email does not match email pattern');
+                error = {
+                    emailError: true,
+                    emailHelperText: 'Invalid email'
+                }
+            }
+        }
+
+        return error;
+    }
+
+    // const passwordInputCheck = () => {
+    //     if (isDelayed) {
+    //         interruptValidationDelay('password')
+    //     }
+    //     isDelayed = true;
+    //     const passwordValidationDelay = setTimeout(() => {
+    //         isDelayed = false;
+    //         return passwordValidation();
+    //     }, delayRate);
+    //     validationDelays = {
+    //         ...validationDelays,
+    //         password: passwordValidationDelay
+    //     }
+    // }
+
+    const passwordValidation = () => {
+        console.log('Validating password...');
+        let error = {};
+        if (!password) {
+            error = {
+                passwordError: true,
+                passwordHelperText: 'Please enter a password'
+            }
+        } else {
+            error = {
+                passwordError: false
+            }
+        }
+        return error;
+    }
+
+    // const interruptValidationDelay = (delayType) => {
+    //     switch(delayType) {
+    //         case 'email':
+    //             clearTimeout(validationDelays.email);
+    //         case 'password':
+    //             clearTimeout(validationDelays.password);
+    //         default:
+    //             clearTimeout();
+    //     }
+    // }
+
+    const isFormInvalid = () => {
+        return !(email && password);
+    }
 
     return (
         <div className='wrapper'>
@@ -40,7 +210,9 @@ const SignIn = (props) => {
                     className='input'
                     name='email'
                     type='email'
-                    placeholder='Email'
+                    label='Email'
+                    error={emailError}
+                    helperText={emailError ? emailHelperText : ''}
                     value={email}
                     onChange={handleChange}
                 />
@@ -48,11 +220,13 @@ const SignIn = (props) => {
                     className='input'
                     name='password'
                     type='password'
-                    placeholder='Password'
+                    label='Password'
+                    error={passwordError}
+                    helperText={passwordError ? passwordHelperText : ''}
                     value={password}
                     onChange={handleChange}
                 />
-                <Button type='submit' label='Login'/>
+                <Button type='submit' label='Login' disabled={isFormInvalid()}/>
             </form>
         </div>
     );
