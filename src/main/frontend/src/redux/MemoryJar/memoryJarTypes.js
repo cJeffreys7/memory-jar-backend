c908bff486ec685401bce3c9a9b500ac143bac57
Chris Jeffreys <chrisbjeffreys@gmail.com>
2022-03-15 14:47:41 -0500

    decouple front end app

--- src/main/frontend/src/redux/MemoryJar/memoryJarTypes.js
+++
@@ -1,6 +0,0 @@
-const memoryJarTypes = {
-    SET_CURRENT_MEMORY_JAR: 'SET_CURRENT_MEMORY_JAR',
-    CLEAR_CURRENT_MEMORY_JAR: 'CLEAR_CURRENT_MEMORY_JAR'
-};
-
-export default memoryJarTypes;
--- src/main/frontend/src/redux/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import userReducer from './User/userReducer';
-import memoryJarReducer from './MemoryJar/memoryJarReducer';
-
-export default combineReducers({
-    user: userReducer,
-    memoryJar: memoryJarReducer
-})
